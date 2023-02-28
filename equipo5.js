a1e1955e85bf3375d32f29a79f4336a0816ce531
Daniel <friasdaniel.1109@gmail.com>
2023-02-27 21:31:46 -0300

    Primer commit

---
+++ equipo5.js
@@ -0,0 +1,51 @@
+/* Integrantes: Ailin Molina, Ariel Ibarbaiz y Daniel Frias */
+/* https://drive.google.com/file/d/19y8MAOxM7seG1VEy6bvvOq0EgnNWVjmM/view */
+
+//-------------------------------
+/* Micro desafio 1
+
+La función test1 retorna 30
+
+*/
+
+//-------------------------------
+/* Micro desafio 2
+
+La función test2 retorna 20
+
+*/
+
+//-------------------------------
+// Funciones simples 1
+const pulgadasEnCentimetros = (pulgadas) => pulgadas * 2.54 + ' centimetros';
+
+// console.log(pulgadasEnCentimetros(0.3));
+
+// Funciones simples 2
+const convertirURL = (texto) => 'https://www.' + texto + '.com';
+
+// console.log(convertirURL('Ailin'));
+
+//-------------------------------
+// Funciones simples 3
+const sorprender = (texto) => texto + '!';
+
+// console.log(sorprender('Ganamos la tercera'));
+
+//-------------------------------
+// Funciones simples 4: Ailin
+
+//-------------------------------
+// Funciones simples 5: Ailin
+
+//-------------------------------
+// Funciones simples 6: Ariel
+
+//-------------------------------
+// Funciones simples 7: Ariel
+
+//-------------------------------
+// Funciones simples 8: Daniel
+
+//-------------------------------
+// Funciones simples 9: Daniel
